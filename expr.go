9b2ab7ff4ca409b4db0b4b7328c2626c47c52165
Joe Axon <joe@jca.io>
2025-02-06 21:59:04 +0000

    Add .Select and .Page for fluent API

--- expr.go
+++ expr.go
@@ -1,8 +1,11 @@
 package sqlj
 
+import "errors"
+
 type QueryDB struct {
 	DB           *DB
 	From         string
+	OrderClauses []OrderBy
 	WhereClauses []WhereClause
 }
 
@@ -62,6 +65,15 @@ func (q QueryDB) OrWhereExpr(expr Expr) QueryDB {
 	return q
 }
 
+func (q QueryDB) Order(expression string, direction string) QueryDB {
+	q.OrderClauses = append(q.OrderClauses, OrderBy{
+		expression: expression,
+		direction:  direction,
+	})
+
+	return q
+}
+
 // Get a record by ID.
 // This will ignore any previous calls to .Where and .OrWhere
 func (q QueryDB) Get(id any, v any) error {
@@ -99,4 +111,59 @@ func (q QueryDB) One(v any, values ...any) error {
 	})
 
 	return q.DB.GetRow(sql, v, values...)
+}
+
+func (q QueryDB) Select(v any, values ...any) error {
+	structInstance, err := getSliceStructInstance(v)
+	if err != nil {
+		return err
+	}
+
+	fields := extractFields(structInstance)
+	columns := pluckNames(fields)
+
+	sql := buildSelectQuery(Select{
+		From:    q.From,
+		Where:   q.WhereClauses,
+		OrderBy: q.OrderClauses,
+		Columns: columns,
+	})
+
+	return q.DB.SelectAll(sql, v)
+}
+
+func (q QueryDB) Page(options PageOptions, v any, values ...any) error {
+	if options.pageNumber < 1 {
+		return errors.New("Page number must be greater than 0")
+	}
+
+	if options.pageSize < 1 {
+		return errors.New("Page size must be greater than 0")
+	}
+
+	if len(options.order) == 0 {
+		return errors.New("Must include atleast one order by")
+	}
+
+	structInstance, err := getSliceStructInstance(v)
+	if err != nil {
+		return err
+	}
+
+	fields := extractFields(structInstance)
+	columns := pluckNames(fields)
+
+	offset := (options.pageNumber - 1) * options.pageSize
+	limit := options.pageSize
+
+	sql := buildSelectQuery(Select{
+		From:    q.From,
+		Where:   q.WhereClauses,
+		OrderBy: options.order,
+		Columns: columns,
+		Offset:  true,
+		Limit:   true,
+	})
+
+	return q.DB.SelectAll(sql, v, offset, limit)
 }
--- sqlj_test.go
+++ sqlj_test.go
@@ -309,11 +309,20 @@ func TestFrom(t *testing.T) {
 		Email: "joe@example.com",
 	}
 
+	userC := User{
+		Name:  "Jane",
+		Email: "jane@example.com",
+	}
+
 	if err := jdb.Insert("user", &userA); err != nil {
 		t.Fatalf("Failed to insert user: %s\n", err.Error())
 	}
 
 	if err := jdb.Insert("user", &userB); err != nil {
+		t.Fatalf("Failed to insert user: %s\n", err.Error())
+	}
+
+	if err := jdb.Insert("user", &userC); err != nil {
 		t.Fatalf("Failed to insert user: %s\n", err.Error())
 	}
 
@@ -333,5 +342,32 @@ func TestFrom(t *testing.T) {
 
 	if foundUserB.ID != userB.ID {
 		t.Fatalf("Fluent API, ID mismatch. Expected: %d, Got: %d\n", userA.ID, foundUserA.ID)
+	}
+
+	var allUsers []User
+	if err := jdb.From("user").Select(&allUsers); err != nil {
+		t.Fatalf("Fluent API, failed to select all: %s\n", err.Error())
+	}
+
+	if len(allUsers) != 3 {
+		t.Fatalf("Fluent API, did not retrieve all users. Expected 3, Got: %d\n", len(allUsers))
+	}
+
+	var sortedUsers []User
+	if err := jdb.From("user").Order("name", "ASC").Select(&sortedUsers); err != nil {
+		t.Fatalf("Fluent API, failed to select all: %s\n", err.Error())
+	}
+
+	if sortedUsers[0].Name != "Jane" {
+		t.Fatalf("Fluent API, sorting failed")
+	}
+
+	var reverseSortedUsers []User
+	if err := jdb.From("user").Order("name", "DESC").Select(&reverseSortedUsers); err != nil {
+		t.Fatalf("Fluent API, failed to select all: %s\n", err.Error())
+	}
+
+	if reverseSortedUsers[0].Name != "Joe" {
+		t.Fatalf("Fluent API, sorting failed")
 	}
 }
